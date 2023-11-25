2775e5e5238b997229f9da9ce83a997ffb24dd5e
Spy_345 <shivyewle705@gmail.com>
2023-11-25 23:23:58 +0530

    Changes in the Login Page

--- my-app/src/pages/login.js
+++ my-app/src/pages/login.js
@@ -2,57 +2,72 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-export default function Login(){
+export default function Login() {
   const [logindata, setLoginData] = useState({});
   const navigate = useNavigate();
 
-  function handleInputChange(event){
-    const {name, value} = event.target;
+  function handleInputChange(event) {
+    const { name, value } = event.target;
 
-    setLoginData(prevData => ({...prevData, [name]: value}));
-  };
+    setLoginData((prevData) => ({ ...prevData, [name]: value }));
+  }
 
-  function handleSubmit(event){
+  function handleSubmit(event) {
     event.preventDefault();
     localStorage.setItem("username", logindata.username);
     localStorage.setItem("password", logindata.password);
 
-    axios.get("/user", {params : logindata})
-    .then(response => 
-      {
+    axios
+      .get("/user", { params: logindata })
+      .then((response) => {
         alert(response.data);
-        if(response.data === "User Authenticated!"){
+        if (response.data === "User Authenticated!") {
           navigate("/home");
-        }else if(response.data === "User Not Found! Please Register"){
+        } else if (response.data === "User Not Found! Please Register") {
           navigate("/");
         }
       })
-    .catch(err => console.log(err));
-    
+      .catch((err) => console.log(err));
+
     document.getElementById("login-form").reset();
     setLoginData({});
   }
 
   return (
     <>
-      <form onSubmit={handleSubmit} id="login-form" className="my-5">
+      <form onSubmit={handleSubmit} id='login-form' className='my-5'>
         <h2>Login</h2>
         <div>
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" placeholder="Enter Your Email" onChange={handleInputChange} required></input>
+          <label htmlFor='username'>Username:</label>
+          <input
+            type='text'
+            id='username'
+            name='username'
+            placeholder='Enter Your Email'
+            onChange={handleInputChange}
+            required
+          ></input>
         </div>
         <div>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" placeholder="Enter Your Password" onChange={handleInputChange} required></input>
+          <label htmlFor='password'>Password:</label>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            placeholder='Enter Your Password'
+            onChange={handleInputChange}
+            required
+          ></input>
         </div>
-        <button className="mx-auto my-3" type="submit">Login</button>
+        <button className='mx-auto my-3 btn btn-lg' type='submit'>
+          Login
+        </button>
       </form>
       <div>
-        <span>Don't have an account?</span><br></br>
-        <Link to="/">Sign Up</Link>
+        <span>Don't have an account?</span>
+        <br></br>
+        <Link to='/'>Sign Up</Link>
       </div>
     </>
-  )
+  );
 }
-
-
--- my-app/src/pages/signup.js
+++ my-app/src/pages/signup.js
@@ -1,70 +1,100 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./style.css"
+import "./style.css";
 
 export default function Signup() {
-
   const [signupdata, setSignupData] = useState({
-    name:"",
-    email:"",
-    password:""
+    name: "",
+    email: "",
+    password: "",
   });
   const navigate = useNavigate();
 
   function handleInputChange(event) {
     const { name, value } = event.target;
 
-    setSignupData(prevData => ({ ...prevData, [name]: value }));
+    setSignupData((prevData) => ({ ...prevData, [name]: value }));
   }
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    axios.post("/signup", signupdata)
-    .then(resposnse => {
-      alert(resposnse.data);
-      if(resposnse.data === "Please Fill The Details!"){
-        navigate("/");
-      }else if(resposnse.data === "User Already Exists!"){
-        navigate("/");
-      } else{
-        navigate("/login"); 
-      }
-    })
-    .catch(err => console.log(err));
-    
+    axios
+      .post("/signup", signupdata)
+      .then((resposnse) => {
+        alert(resposnse.data);
+        if (resposnse.data === "Please Fill The Details!") {
+          navigate("/");
+        } else if (resposnse.data === "User Already Exists!") {
+          navigate("/");
+        } else {
+          navigate("/login");
+        }
+      })
+      .catch((err) => console.log(err));
+
     document.getElementById("signup-form").reset();
     setSignupData({
-      name:"",
-    email:"",
-    password:""
+      name: "",
+      email: "",
+      password: "",
     });
   }
 
   return (
     <>
-        <form  onSubmit={handleSubmit} className="my-5 mx-auto" id="signup-form">
-          <h2>Sign Up</h2>
-            <div>
-              <label for="name">Name:</label>
-              <input type="text"  id="name" name="name" className="form-group mx-auto" placeholder="Enter Your Name" onChange={handleInputChange} required></input>
-            </div>
-            <div><label for="email">Email:</label>
-              <input type="email" id="email" name="email" className="form-group mx-auto" placeholder="Email" onChange={handleInputChange} required></input>
-            </div>
-            <div>
-              <label for="password">Password:</label>
-              <input type="password" id="password" name="password" className="form-group mx-auto" placeholder="Password" onChange={handleInputChange} required></input>
-            </div>
-            <button type="submit" onClick={handleSubmit} className="btn btn-secondary">Sign Up</button>
-        </form>
-        <div className="my-auto position-relative mx-auto">
-          <span>Already have an account?</span><br></br>
-          <Link to="/login">Login</Link>
+      <form onSubmit={handleSubmit} className='my-5 mx-auto' id='signup-form'>
+        <h2>Sign Up</h2>
+        <div>
+          <label htmlFor='name'>Name:</label>
+          <input
+            type='text'
+            id='name'
+            name='name'
+            className='form-group mx-auto'
+            placeholder='Enter Your Name'
+            onChange={handleInputChange}
+            required
+          ></input>
         </div>
+        <div>
+          <label htmlFor='email'>Email:</label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            className='form-group mx-auto'
+            placeholder='Email'
+            onChange={handleInputChange}
+            required
+          ></input>
+        </div>
+        <div>
+          <label htmlFor='password'>Password:</label>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            className='form-group mx-auto'
+            placeholder='Password'
+            onChange={handleInputChange}
+            required
+          ></input>
+        </div>
+        <button
+          type='submit'
+          onClick={handleSubmit}
+          className='btn btn-secondary mt-2 btn-lg'
+        >
+          Sign Up
+        </button>
+      </form>
+      <div className='my-auto position-relative mx-auto'>
+        <span>Already have an account?</span>
+        <br></br>
+        <Link to='/login'>Login</Link>
+      </div>
     </>
-  )
+  );
 }
-
-
